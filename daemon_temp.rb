37dcf09d33f43ce46586209e9ff4c00fdf6b18d7
Javier Diaz <javierdiazt406@gmail.com>
2024-04-03 11:43:09 -0400

    Fix daemon

--- daemon_temp.rb
+++ daemon_temp.rb
@@ -7,13 +7,14 @@ $redis.psubscribe('__keyevent@0__:expired') do |on|
   on.pmessage do |pattern, event, key|
   $event = key.split('_').first.to_s
   case $event
-  when "auto:temp"
-    Archive.stat
+    when "auto:temp"
+      Archive.stat
 
-    url = 'https://temp.rifa-max.com/refresh'
+      url = 'https://temp.rifa-max.com/refresh'
 
-    HTTParty.get(url)
-  else
-    puts "Event has not been defined: #{$event}. Events defined are: auto:temp"
+      HTTParty.get(url)
+    else
+      puts "Event has not been defined: #{$event}. Events defined are: auto:temp"
+    end
   end
 end
