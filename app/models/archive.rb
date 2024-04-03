8ed9b43ba1f4ad261e46403f9b24fb2013baead3
Javier Diaz <javierdiazt406@gmail.com>
2024-04-03 11:08:39 -0400

    Almost ready

--- app/models/archive.rb
+++ app/models/archive.rb
@@ -21,78 +21,80 @@ class Archive < ApplicationRecord
 
     @device = Device.find_by(token: device_token)
 
-    if @device.nil? raise { message: "Device not found" }
-    
-    minute_parse = minute.to_i < 30 ? "00" : "30"
+    if @device.nil? 
+      raise { message: "Device not found" } 
+    else
+      minute_parse = minute.to_i < 30 ? "00" : "30"
 
-    temperatures = {
-      "09:30": 28.9,
-      "10:00": 29.1,
-      "10:30": 29.4,
-      "11:00": 29.5,
-      "11:30": 29.7,
-      "12:00": 29.8,
-      "12:30": 29.9,
-      "13:00": 30.2,
-      "13:30": 31.3,
-      "14:00": 31.9,
-      "14:30": 32.1,
-      "15:00": 32.5,
-      "15:30": 32.7,
-      "16:00": 32.8,
-      "16:30": 32.4,
-      "17:00": 32.1,
-      "17:30": 31.8,
-      "18:00": 31.5,
-      "18:30": 31.2,
-      "19:00": 31.1,
-      "19:30": 31.1,
-      "20:00": 30.9,
-      "20:30": 30.7,
-      "21:00": 30.3,
-    }
+      temperatures = {
+        "09:30": 28.9,
+        "10:00": 29.1,
+        "10:30": 29.4,
+        "11:00": 29.5,
+        "11:30": 29.7,
+        "12:00": 29.8,
+        "12:30": 29.9,
+        "13:00": 30.2,
+        "13:30": 31.3,
+        "14:00": 31.9,
+        "14:30": 32.1,
+        "15:00": 32.5,
+        "15:30": 32.7,
+        "16:00": 32.8,
+        "16:30": 32.4,
+        "17:00": 32.1,
+        "17:30": 31.8,
+        "18:00": 31.5,
+        "18:30": 31.2,
+        "19:00": 31.1,
+        "19:30": 31.1,
+        "20:00": 30.9,
+        "20:30": 30.7,
+        "21:00": 30.3,
+      }
 
-    humidity = {
-      "09:30": 73.0,
-      "10:00": 71.0,
-      "10:30": 71.2,
-      "11:00": 70.9,
-      "11:30": 70.3,
-      "12:00": 69.1,
-      "12:30": 69.5,
-      "13:00": 69.4,
-      "13:30": 68.1,
-      "14:00": 67.6,
-      "14:30": 67.1,
-      "15:00": 65.1,
-      "15:30": 63.1,
-      "16:00": 62.3,
-      "16:30": 61.1,
-      "17:00": 62.7,
-      "17:30": 61.2,
-      "18:00": 63.4,
-      "18:30": 65.1,
-      "19:00": 64.2,
-      "19:30": 65.7,
-      "20:00": 67.3,
-      "20:30": 70.3,
-      "21:00": 71.5,
-    }
+      humidity = {
+        "09:30": 73.0,
+        "10:00": 71.0,
+        "10:30": 71.2,
+        "11:00": 70.9,
+        "11:30": 70.3,
+        "12:00": 69.1,
+        "12:30": 69.5,
+        "13:00": 69.4,
+        "13:30": 68.1,
+        "14:00": 67.6,
+        "14:30": 67.1,
+        "15:00": 65.1,
+        "15:30": 63.1,
+        "16:00": 62.3,
+        "16:30": 61.1,
+        "17:00": 62.7,
+        "17:30": 61.2,
+        "18:00": 63.4,
+        "18:30": 65.1,
+        "19:00": 64.2,
+        "19:30": 65.7,
+        "20:00": 67.3,
+        "20:30": 70.3,
+        "21:00": 71.5,
+      }
 
-    current_temp = temperatures[:"#{hour}:#{minute_parse}"]
-    current_hum = humidity[:"#{hour}:#{minute_parse}"]
+      current_temp = temperatures[:"#{hour}:#{minute_parse}"]
+      current_hum = humidity[:"#{hour}:#{minute_parse}"]
 
-    thermal_sensation = (0.994 * current_temp) + (0.0553 * current_hum)
+      thermal_sensation = (0.994 * current_temp) + (0.0553 * current_hum)
 
-    $redis.setex("auto:temp_daemon", 30.minutes.to_i, 1)
-    
-    Log.create(
-      temperature: current_temp,
-      humidity: current_hum,
-      thermal_sensation: thermal_sensation.round(2),
-      degree: 'C',
-      device_id: 1,
-      archive_id: Archive.last.id
-    )
+      $redis.setex("auto:temp_daemon", 30.minutes.to_i, 1)
+      
+      Log.create(
+        temperature: current_temp,
+        humidity: current_hum,
+        thermal_sensation: thermal_sensation.round(2),
+        degree: 'C',
+        device_id: 1,
+        archive_id: Archive.last.id
+      )
+    end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,7 @@
 Device.create(model: 'DHT11', is_active: true, token: "df0b8765-a111-44d2-946f-9e6644df985b")
 
 Archive.create(current: Date.yesterday, device_token: "df0b8765-a111-44d2-946f-9e6644df985b")
+
+Log.create(degree: "C", humidity: "70.3", temperature: "30.7", thermal_sensation: "32.1", archive_id: 1, device_id: 1, created_at: Time.now - 1.day + 90.minutes, updated_at: Time.now - 1. + 90.minutes)
+Log.create(degree: "C", humidity: "65.6", temperature: "31.2", thermal_sensation: "33.2", archive_id: 1, device_id: 1, created_at: Time.now - 1.day + 60.minutes, updated_at: Time.now - 1.day + 60.minutes)
+Log.create(degree: "C", humidity: "67.1", temperature: "31.6", thermal_sensation: "33.5", archive_id: 1, device_id: 1, created_at: Time.now - 1.day + 30.minutes, updated_at: Time.now - 1.day + 30.minutes)
