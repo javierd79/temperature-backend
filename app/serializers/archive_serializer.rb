6976cfe27dd901aa502ac96ae37a4ab922c08086
Javier Diaz <javierdiazt406@gmail.com>
2024-04-03 12:14:14 -0400

    Init

--- app/serializers/archive_serializer.rb
+++ app/serializers/archive_serializer.rb
@@ -1,5 +1,16 @@
 class ArchiveSerializer < ActiveModel::Serializer
-  attributes :id, :current, :device_token, :created_at
+  attributes :id, :current, :device_token, :created_at, :logs
 
-  has_many :logs
+  def logs
+    object.logs.map do |log|
+      {
+        id: log.id,
+        temperature: log.temperature,
+        humidity: log.humidity,
+        thermal_sensation: log.thermal_sensation,
+        degree: log.degree,
+        created_at: log.created_at
+      }
+    end
+  end
 end
